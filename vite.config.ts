f086da8c10694daf7bc6e3233f994417f7aafa73
tomasfqit <tomas.quichimbo@tomebamba.com.ec>
2025-07-30 16:32:28 -0500

    INIT COMMIT

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import { defineConfig } from "vite";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/plantilla-muestra-itsa/",
   plugins: [react()],
   resolve: {
     alias: {
